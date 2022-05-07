fb86e2e33b2022fdc27de1691bd06e8e0f65de92
ashikurcmt126 <56148949+ashikurcmt126@users.noreply.github.com>
2022-05-07 14:43:05 +0600

    Create Duplicate Emails.sql

---
+++ Easy/Duplicate Emails.sql
@@ -0,0 +1,26 @@
+
+-- create
+CREATE TABLE Person (
+  Id int primary key,
+  Email varchar(20)
+  
+);
+
+
+INSERT INTO Person(Id, Email)
+VALUES
+	(1, 'a@b.com'),
+	(2, 'c@d.com'),
+	(3, 'a@b.com');
+
+
+select * from Person;	
+
+select d.Email from 
+(
+select Email, count(*) as counter
+from Person 
+group by Email
+) d
+where counter >1 
+group by d.Email;
