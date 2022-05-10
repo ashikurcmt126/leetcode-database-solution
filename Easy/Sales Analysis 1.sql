0335aad47b28bf3865e43c78b2d21535eec4da41
ashikurcmt126 <56148949+ashikurcmt126@users.noreply.github.com>
2022-05-10 08:11:12 +0600

    Create Sales Analysis 1.sql

---
+++ Easy/Sales Analysis 1.sql
@@ -0,0 +1,45 @@
+
+-- create
+CREATE TABLE Product (
+  product_id int,
+  product_name varchar(20),
+  unit_price int
+);
+
+-- create
+CREATE TABLE Sales (
+  seller_id int,
+  product_id int,
+  buyer_id int,
+  sale_date date,
+  quantity int,
+  price int
+);
+
+
+
+
+INSERT INTO Product(product_id, product_name, unit_price)
+VALUES
+	(1, 'S8', 1000),
+	(2, 'G4', 800),
+	(3, 'iPhone', 1400);
+	
+INSERT INTO Sales(seller_id, product_id, buyer_id, sale_date, quantity, price)
+VALUES
+	(1, 1, 1, '2019-01-21', 2, 2000),
+	(1, 2, 2, '2019-02-17', 1, 800),
+	(2, 2, 3, '2019-06-02', 1, 800),
+	(3, 3, 4, '2019-05-13', 2, 2800);
+
+
+select * from Product;
+select * from Sales;
+
+select seller_id from
+(
+select seller_id, rank() over(order by sum(price) desc) as rk
+from Sales 
+group by seller_id
+)d 
+where d.rk = 1
